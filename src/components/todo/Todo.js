8d90c0740d0157915eecf2b8a2bc7c93a8b6210a
KevinFrancis-11 <kevinfrancis967@gmail.com>
2023-09-30 22:53:32 +0530

    Initial Commit

---
+++ src/components/todo/Todo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Todo() {
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>Todo component/home component</h1>
+        </div>
+    );
+}
+
+export default Todo;
